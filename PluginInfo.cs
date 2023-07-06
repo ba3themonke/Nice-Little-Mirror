bf0859aca93a8dfe17b36feba8f82261c4b14d9f
ba3 <138734481+ba3themonke@users.noreply.github.com>
2023-07-05 20:12:53 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace NiceLittleMirror
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.ba3.gorillatag.nicelittlemirror";
+        public const string Name = "NiceLittleMirror";
+        public const string Version = "1.0.0";
+    }
+}
